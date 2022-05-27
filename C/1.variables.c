35d4e63f92ee1c981824734b5394ac9c83d54c4d
jorgemaor <jorgemaor98@gmail.com>
2022-05-27 07:59:40 -0500

    aprediendo lo basico de C, como funcionan los tipos de datos y como implementarlos

---
+++ C/1.variables.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+
+int main ()
+{
+  int integerA = 1;
+  float floatA = 1.1;
+  double doubleA = 1.2;
+  char letterA = 'A';
+
+  //en las siguientes lineas de codigo voy a imprimir todas mis variables
+  printf("El valor de nuestro numero es: %i \n", integerA);
+  printf("el valor de nuestra variable es: %f \n", floatA);
+  printf("el valor de nuestro double es: %f \n", doubleA);
+  printf("el valor de nuestra letter es: %c \n", letterA);
+   
+  return 0;
+}
